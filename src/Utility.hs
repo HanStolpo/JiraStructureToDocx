73be50ed34eaccbbe83306beb384a2f762cf8d4c
Handre Stolp <HanStolpo@gmail.com>
2013-12-11 15:51:26 +0200

    Warning and removal

--- src/Utility.hs
+++ src/Utility.hs
@@ -2,12 +2,12 @@
 module Utility   ( prettyJson
                     ) where
 
-import Data.ByteString.Lazy.Char8 (ByteString, unpack, pack)     -- only import string instances for overloaded strings
+import Data.ByteString.Lazy.Char8 (ByteString, unpack)     -- only import string instances for overloaded strings
 import qualified Data.ByteString.Lazy.Char8 as C 
 import qualified Data.Char as C
 import Data.ByteString.Lazy.Builder
 import Data.Monoid
-import Data.Default
+--import Data.Default
 
 
 prettyJson :: C.ByteString -> C.ByteString
@@ -23,13 +23,13 @@ prettyJson = toLazyByteString . toB
         f (b, (pfx, False)) (p, c) = let (pfx', pfx'') = m c pfx in (b <> nl p c pfx', (pfx'', fis p c False)) 
         f (b, (pfx, True)) (p, c)  = (b <> char8 c, (pfx, fis p c True)) 
         -- mod the pfx
-        m c pfx | c `elem` "{[" = (pfx, (' ':' ':pfx))
-        m c pfx@(_:_:pfx') | c `elem` "}]" = (pfx', pfx')
+        m c pfx | c `elem` "{[" = (pfx, ' ':' ':pfx)
+        m c (_:_:pfx') | c `elem` "}]" = (pfx', pfx')
         m _ pfx = (pfx, pfx) 
         -- new line choice
         nl :: Char -> Char -> String -> Builder
         nl p c pfx | p /= ',' && c `elem` "{[]}" = char8 '\n' <> string8 pfx <> char8 c
-        nl p c pfx | p == ',' && not (c `elem` "{[]}") = char8 '\n' <> string8 pfx <> char8 c
+        nl p c pfx | p == ',' && notElem c "{[]}" = char8 '\n' <> string8 pfx <> char8 c
         nl _ c _ = char8 c
         -- flip in string status
         fis '\\' '"' b = b
