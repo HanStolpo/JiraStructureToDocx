1c52d5cbbb69ac193a0739e185c36d6092c5cdc0
Handre Stolp <HanStolpo@gmail.com>
2013-12-11 15:54:03 +0200

    Warning removal

--- src/IssueHierarchy.hs
+++ src/IssueHierarchy.hs
@@ -22,9 +22,9 @@ import Test.Framework
 import Test.Framework.Providers.HUnit
 import Test.Framework.TH
 import qualified Data.Aeson as AS
-import Text.RawString.QQ
+-- import Text.RawString.QQ
 
-import Data.ByteString.Lazy.Char8 (ByteString, unpack)     -- only import string instances for overloaded strings
+import Data.ByteString.Lazy.Char8 (unpack)     -- only import string instances for overloaded strings
 import Debug.Trace
 
 import JiraTypes
@@ -43,7 +43,7 @@ instance AS.ToJSON IssueHierarchy
 instance AS.FromJSON IssueHierarchy 
 
 case_serializeHierarchy :: Assertion
-case_serializeHierarchy = (Just h) @=? (AS.decode . (\s -> trace ("\n" ++ (unpack s) ++ "\n") s) . prettyJson . AS.encode $ h)
+case_serializeHierarchy = Just h @=? (AS.decode . (\s -> trace ("\n" ++ unpack s ++ "\n") s) . prettyJson . AS.encode $ h)
     where 
         h = IssueHierarchyRoot
             {
@@ -63,7 +63,7 @@ case_serializeHierarchy = (Just h) @=? (AS.decode . (\s -> trace ("\n" ++ (unpac
                                                 attUri = "attUri",
                                                 attFileName = "attFileName"
                                                 }],
-                            issueLinks = [(Outward "Blah" 1 "Blah"), (Inward "Blahs" 2 "Blahs")]
+                            issueLinks = [Outward "Blah" 1 "Blah", Inward "Blahs" 2 "Blahs"]
                         },
                         ihChildren = def
                     },
@@ -81,7 +81,7 @@ case_serializeHierarchy = (Just h) @=? (AS.decode . (\s -> trace ("\n" ++ (unpac
                                                 attUri = "2attUri",
                                                 attFileName = "2attFileName"
                                                 }],
-                            issueLinks = [(Outward "2Blah" 1 "2Blah"), (Inward "2Blahs" 2 "2Blahs")]
+                            issueLinks = [Outward "2Blah" 1 "2Blah", Inward "2Blahs" 2 "2Blahs"]
                         },
                         ihChildren = def
                     }
