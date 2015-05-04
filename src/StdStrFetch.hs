6de5f2f8d9e936c106a46e05c0e037e9b5128c6b
Handre Stolp <HanStolpo@gmail.com>
2015-05-04 10:11:10 +0200

    exported fetchStrTestSrc

--- src/StdStrFetch.hs
+++ src/StdStrFetch.hs
@@ -4,6 +4,7 @@
 module StdStrFetch  (fetchStdSrc
                     ,fetchStrSrc
                     ,fetchCycleIdFromName
+                    ,fetchStrTestSrc
                     ,fetchCycleExecutions
                     ) where
 import GHC.Exts
